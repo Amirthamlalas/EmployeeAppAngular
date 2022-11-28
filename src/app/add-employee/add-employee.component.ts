4fd8619a5f0c7fa80a697b86a4d1a8f8fa8d5e87
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-28 12:39:33 +0530

    read values

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent {
+  name=""
+  Designation=""
+  Phone=""
+  Salary=""
 
+
+  readValue = ()=>
+  {
+    let data : any ={
+      "name" : this.name,
+      "Designation" : this.Designation,
+      "Phone"  : this.Phone,
+      "Salary" : this.Salary
+      
+  }
+  console.log(data)
+  }
 }
