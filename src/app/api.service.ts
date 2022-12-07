c9ff34d26bf17738cb2c1315f8c235d44bfadb33
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-07 15:36:19 +0530

    added apiintegration

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -16,4 +16,8 @@ export class ApiService {
   addCourse=(dataToSend:any)=>{
     return this.http.post("http://localhost:8080/add",dataToSend)
   }
+
+  searchData=(dataToSend:any)=>{
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
 }
